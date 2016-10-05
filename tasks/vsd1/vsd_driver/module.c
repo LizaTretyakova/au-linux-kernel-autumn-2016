dfdd55e7535207125afd2dd2aa6d27b200fb55d7
LizaTretyakova <elizabet.tretyakova@gmail.com>
2016-10-05 22:27:00 +0300

    backup
    solution

--- tasks/vsd1/vsd_driver/module.c
+++ tasks/vsd1/vsd_driver/module.c
@@ -20,31 +20,33 @@ typedef struct vsd_dev {
 } vsd_dev_t;
 static vsd_dev_t *vsd_dev;
 
-static int vsd_dev_open(struct inode *inode, struct file *filp)
-{
+static int vsd_dev_open(struct inode *inode, struct file *filp) {
     pr_notice(LOG_TAG "opened\n");
     return 0;
 }
 
-static int vsd_dev_release(struct inode *inode, struct file *filp)
-{
+static int vsd_dev_release(struct inode *inode, struct file *filp) {
     pr_notice(LOG_TAG "closed\n");
     return 0;
 }
 
 static ssize_t vsd_dev_read(struct file *filp,
-    char __user *read_user_buf, size_t read_size, loff_t *fpos)
-{
-    // TODO copy VSD data from *fpos to *fpos + read_size
-    // to read_user_buf. Advance *fpos (current r/w position of opened file).
-    // Handle all errors. Return proper value that read syscall should return.
-    // See man 2 read, Linux device drivers 3 book (char devices chanpter).
-    return 0;
+    char __user *read_user_buf, size_t read_size, loff_t *fpos) {
+    if (*fpos > vsd_dev->buf_size)
+        return -EINVAL;
+
+    if (*fpos + read_size >= vsd_dev->buf_size)
+        read_size = vsd_dev->buf_size - *fpos;
+
+    if (copy_to_user(read_user_buf, vsd_dev->vbuf + *fpos, read_size))
+        return -EFAULT;
+
+    *fpos += read_size;
+    return read_size;
 }
 
 static ssize_t vsd_dev_write(struct file *filp,
-    const char __user *write_user_data, size_t write_size, loff_t *fpos)
-{
+    const char __user *write_user_data, size_t write_size, loff_t *fpos) {
     if (*fpos >= vsd_dev->buf_size)
         return -EINVAL;
 
@@ -58,8 +60,7 @@ static ssize_t vsd_dev_write(struct file *filp,
     return write_size;
 }
 
-static loff_t vsd_dev_llseek(struct file *filp, loff_t off, int whence)
-{
+static loff_t vsd_dev_llseek(struct file *filp, loff_t off, int whence) {
     loff_t newpos = 0;
 
     switch(whence) {
@@ -83,15 +84,16 @@ static loff_t vsd_dev_llseek(struct file *filp, loff_t off, int whence)
     return newpos;
 }
 
-static long vsd_ioctl_get_size(vsd_ioctl_get_size_arg_t __user *uarg)
-{
-    //TODO return vsd_dev->buf_size in uarg
-    // set proper return value
+static long vsd_ioctl_get_size(vsd_ioctl_get_size_arg_t __user *uarg) {
+    vsd_ioctl_get_size_arg_t arg;
+    arg.size = vsd_dev->buf_size;
+    if (copy_to_user(uarg, &arg, sizeof(arg)))
+        return -EFAULT;
+
     return 0;
 }
 
-static long vsd_ioctl_set_size(vsd_ioctl_set_size_arg_t __user *uarg)
-{
+static long vsd_ioctl_set_size(vsd_ioctl_set_size_arg_t __user *uarg) {
     vsd_ioctl_set_size_arg_t arg;
     if (copy_from_user(&arg, uarg, sizeof(arg)))
         return -EFAULT;
@@ -99,12 +101,14 @@ static long vsd_ioctl_set_size(vsd_ioctl_set_size_arg_t __user *uarg)
     if (arg.size <= vsd_dev->buf_size) {
         vsd_dev->buf_size = arg.size;
         return 0;
-    } else return -ENOMEM;
+    } else {
+        printk(KERN_ALERT "arg.size: %zd\n vsd_dev->buf_size: %zd\n", arg.size, vsd_dev->buf_size);
+        return -ENOMEM;
+    }
 }
 
 static long vsd_dev_ioctl(struct file *filp, unsigned int cmd,
-        unsigned long arg)
-{
+        unsigned long arg) {
     switch(cmd) {
         case VSD_IOCTL_GET_SIZE:
             return vsd_ioctl_get_size((vsd_ioctl_get_size_arg_t __user*)arg);
@@ -121,25 +125,21 @@ static struct file_operations vsd_dev_fops = {
     .owner = THIS_MODULE,
     .open = vsd_dev_open,
     .release = vsd_dev_release,
-    // TODO to export all the VSD functionality to userspace
-    // set proper pointers to functions here.
-    .read = NULL,
-    .write = NULL,
-    .llseek = NULL,
-    .unlocked_ioctl = NULL
+    .read = vsd_dev_read,
+    .write = vsd_dev_write,
+    .llseek = vsd_dev_llseek,
+    .unlocked_ioctl = vsd_dev_ioctl
 };
 
 #undef LOG_TAG
 #define LOG_TAG "[VSD_DRIVER] "
 
-static int vsd_driver_probe(struct platform_device *pdev)
-{
+static int vsd_driver_probe(struct platform_device *pdev) {
     int ret = 0;
     struct resource *vsd_phy_mem_buf_res = NULL;
     pr_notice(LOG_TAG "probing for device %s\n", pdev->name);
 
-    //TODO alloc memory for driver here
-    vsd_dev = (vsd_dev_t*)NULL;
+    vsd_dev = (vsd_dev_t*)kzalloc(sizeof(vsd_dev_t), GFP_KERNEL);
     if (!vsd_dev) {
         ret = -ENOMEM;
         pr_warn(LOG_TAG "Can't allocate memory\n");
@@ -147,9 +147,7 @@ static int vsd_driver_probe(struct platform_device *pdev)
     }
     vsd_dev->mdev.minor = MISC_DYNAMIC_MINOR;
     vsd_dev->mdev.name = "vsd";
-    // TODO set pointer to valid file_operations that exports
-    // VSD functionality to userspace
-    vsd_dev->mdev.fops = NULL;
+    vsd_dev->mdev.fops = &vsd_dev_fops;
     vsd_dev->mdev.mode = S_IRUSR | S_IRGRP | S_IROTH
         | S_IWUSR| S_IWGRP | S_IWOTH;
 
@@ -163,9 +161,12 @@ static int vsd_driver_probe(struct platform_device *pdev)
         goto error_get_buf;
     }
     vsd_dev->vbuf = phys_to_virt(vsd_phy_mem_buf_res->start);
-    //TODO compute VSD buffer size using information from
-    // abstract memory resource vsd_phy_mem_buf_res
-    vsd_dev->buf_size = 0;
+    if (vsd_phy_mem_buf_res->end < vsd_phy_mem_buf_res->start) {
+        ret = -ENOMEM;
+        pr_warn(LOG_TAG "Mixed up end and start. REMOVE THIS!");
+        goto error_get_buf;
+    }
+    vsd_dev->buf_size = vsd_phy_mem_buf_res->end - vsd_phy_mem_buf_res->start;
 
     pr_notice(LOG_TAG "VSD dev with MINOR %u"
         " has started successfully\n", vsd_dev->mdev.minor);
@@ -180,12 +181,10 @@ error_alloc:
     return ret;
 }
 
-static int vsd_driver_remove(struct platform_device *dev)
-{
+static int vsd_driver_remove(struct platform_device *dev) {
     pr_notice(LOG_TAG "removing device %s\n", dev->name);
-    //TODO vsd_driver module is unloaded here.
-    // so we need to unregister and cleanup
-    // misc_device owned by vsd_driver module
+    misc_deregister(&vsd_dev->mdev);
+    kfree(vsd_dev);
     return 0;
 }
 
@@ -193,25 +192,16 @@ static struct platform_driver vsd_driver = {
     .probe = vsd_driver_probe,
     .remove = vsd_driver_remove,
     .driver = {
-        //TODO set platform_driver name here.
-        // platform_bus matches its devices and drivers using
-        // driver name and device name. platform_bus calls driver probe method
-        // when it finds driver with the same name as new device name.
-        .name = "",
+        .name = "au-vsd",
         .owner = THIS_MODULE,
     }
 };
 
-static int __init vsd_driver_init(void)
-{
-    //TODO use platform_driver_register to notify platform_bus
-    // that we have platform_driver (vsd_driver).
-    // Also set proper return code.
-    return -EINVAL;
+static int __init vsd_driver_init(void) {
+    return platform_driver_register(&vsd_driver);
 }
 
-static void __exit vsd_driver_exit(void)
-{
+static void __exit vsd_driver_exit(void) {
     platform_driver_unregister(&vsd_driver);
 }
 
--- tasks/vsd1/vsd_userspace/main.c
+++ tasks/vsd1/vsd_userspace/main.c
@@ -1,7 +1,111 @@
-/*
- * TODO parse command line arguments and call proper
- * VSD_IOCTL_* using C function 'ioctl' (see man ioctl).
- */
-int main(int argc, char **argv) {
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include "../vsd_driver/vsd_ioctl.h"
+
+#define SIZE_GET "size_get"
+#define SIZE_SET "size_set"
+#define DEVICE_FILE_NAME "/dev/vsd"
+
+static int read_size_arg(size_t* size, char* startptr) {
+    char* endptr;
+    long int new_size;
+
+    errno = 0;
+    new_size = strtol(startptr, &endptr, 10);
+    if ((errno == ERANGE && (new_size == LONG_MAX || new_size == LONG_MIN))
+                || (errno != 0 && new_size == 0)) {
+        perror("Invalid size argument");
+        return -1;
+    }
+    if (new_size < 0 || new_size > SIZE_MAX || *endptr != 0) {
+        printf("Invalid size argument! New size: %ld. *endptr: %d\n", new_size, *endptr);
+        return -1;
+    }
+
+    *size = new_size;
     return 0;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Not enough arguments!\n");
+        return EXIT_FAILURE;
+    }
+    if (argc == 2) {
+        if (strcmp(argv[1], SIZE_GET) == 0) {
+            vsd_ioctl_get_size_arg_t arg;
+            int filp;
+            int res;
+
+            filp = open(DEVICE_FILE_NAME, 0);
+            if (filp < 0) {
+                perror("Failed to open VSD");
+                return EXIT_FAILURE;
+            }
+
+            res = ioctl(filp, VSD_IOCTL_GET_SIZE, &arg);
+            if (res < 0) {
+                perror("Failed to get the VSD size");
+                close(filp);
+                return EXIT_FAILURE;
+            }
+
+            printf("The size of the device is %zd.\n", arg.size);
+            close(filp);
+
+            return EXIT_SUCCESS;
+        } else {
+            if (strcmp(argv[1], SIZE_SET) == 0) {
+                printf("Invalid argument: you need to specify the desired size.\n");
+            } else {
+                printf("Invalid argument. Try again!\n");
+            }
+
+            return EXIT_FAILURE;
+        }
+    }
+    if (argc == 3) {
+        // The code below is slightly different from the GET-version
+        // so I don't think it's worth extracting it into a function.
+        if (strcmp(argv[1], SIZE_SET) == 0) {
+            vsd_ioctl_set_size_arg_t arg;
+            int filp;
+            int res;
+
+            if (read_size_arg(&(arg.size), argv[2]) < 0) {
+                return EXIT_FAILURE;
+            }
+
+            filp = open(DEVICE_FILE_NAME, 0);
+            if (filp < 0) {
+                perror("Failed to open VSD");
+                return EXIT_FAILURE;
+            }
+
+            res = ioctl(filp, VSD_IOCTL_SET_SIZE, &arg);
+            if (res < 0) {
+                perror("Failed to get the VSD size");
+                close(filp);
+                return EXIT_FAILURE;
+            }
+
+            printf("The new size of the device is now %zd.\n", arg.size);
+            close(filp);
+
+            return EXIT_SUCCESS;
+        } else {
+            printf("Invalid argument. Try again!\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_FAILURE;
 }
