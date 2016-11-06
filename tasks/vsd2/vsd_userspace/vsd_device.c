e676a7283d090ec3863c4715cae09c65952081f5
LizaTretyakova <elizabet.tretyakova@gmail.com>
2016-11-06 16:56:14 +0300

    Tests pass.

--- tasks/vsd2/vsd_userspace/vsd_device.c
+++ tasks/vsd2/vsd_userspace/vsd_device.c
@@ -73,9 +73,16 @@ int vsd_set_size(size_t size)
 
 ssize_t vsd_read(char* dst, off_t offset, size_t size)
 {
+    off_t res_seek = (off_t) -1;
     CHECK_STATE_CORRECTNESS(err)
 
-    res = read(filp, dst + offset, size);
+    res_seek = lseek(filp, offset, SEEK_SET);
+    err = errno;
+    if(res_seek == (off_t) -1) {
+        return -err;
+    }
+
+    res = read(filp, dst, size);
     err = errno;
     if(res < 0) {
         return -err;
@@ -86,12 +93,17 @@ ssize_t vsd_read(char* dst, off_t offset, size_t size)
 
 ssize_t vsd_write(const char* src, off_t offset, size_t size)
 {
+    off_t res_seek = (off_t) -1;
     CHECK_STATE_CORRECTNESS(err)
 
-    printf("VSD_WRITE: src %p, offt %zu, size %zu\n", src, offset, size);
-    res = write(filp, src + offset, size);
+    res_seek = lseek(filp, offset, SEEK_SET);
     err = errno;
-    printf("%d\n", res);
+    if(res_seek == (off_t) -1) {
+        return -err;
+    }
+
+    res = write(filp, src, size);
+    err = errno;
     if(res < 0) {
         return -err;
     }
@@ -105,14 +117,9 @@ void* vsd_mmap(size_t offset)
     void* ret;
     CHECK_STATE_CORRECTNESS(NULL)
 
-    printf("VSD_MMAP: offset %zu\n", offset);
-
     if(vsd_get_size(&size) || offset % sysconf(_SC_PAGE_SIZE)) {
         return NULL;
     }
-
-    printf("VSD_MMAP: size %zu\n", size);
-    printf("VSD_MMAP: filp %d\n", filp);
 
     ret = mmap(NULL, size - offset,
                PROT_EXEC | PROT_READ | PROT_WRITE, MAP_SHARED,
@@ -132,5 +139,5 @@ int vsd_munmap(void* addr, size_t offset)
         return -1;
     }
 
-    return munmap(addr,  - offset);
+    return munmap(addr, size - offset);
 }
